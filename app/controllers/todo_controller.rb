6cf67babcbf1c066228a9247926c9ef8feb2b6fc
Simon Henrotte <henrotte.simon@gmail.com>
2014-05-20 08:45:33 +0200

    Resolve issues to make pullreview happy

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,8 +1,8 @@
 class TodoController < ApplicationController
 
     def index
-        @todos = Todo.where(done:false)
-        @todone = Todo.where(done:true)
+        @todos = Todo.where(done: false)
+        @todone = Todo.where(done: true)
     end
 
     def show
@@ -14,14 +14,14 @@ class TodoController < ApplicationController
     end
 
     def todo_params
-        params.require(:todo).permit(:name,:description, :done)
+        params.require(:todo).permit(:name, :description, :done)
     end
 
     def create
         @todo = Todo.new(todo_params)
 
         if @todo.save
-            redirect_to todo_index_path, :success => "Your todo has been created with success!"
+            redirect_to todo_index_path, :success => 'Your todo has been created with success!'
         else
             render "new"
         end
@@ -31,9 +31,9 @@ class TodoController < ApplicationController
         @todo = Todo.find(params[:id])
 
         if @todo.update_attribute(:done, true)
-            redirect_to todo_index_path, :notice => "Your todo has been set to done"
+            redirect_to todo_index_path, :notice => 'Your todo has been set to done'
         else
-            redirect_to todo_index_path, :notice => "Your todo has not been marked has done"
+            redirect_to todo_index_path, :notice => 'Your todo has not been marked has done'
         end
     end
 
@@ -41,7 +41,7 @@ class TodoController < ApplicationController
         @todo = Todo.find(params[:id])
         @todo.destroy
 
-        redirect_to todo_index_path, :notice => "Your todo has been removed with success!"
+        redirect_to todo_index_path, :notice => 'Your todo has been removed with success!'
     end
 
 end
